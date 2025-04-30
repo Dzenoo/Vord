b8ab786fc70d4428469ab19e40438803406d3111
Dzenis <dzenisgudzevic18@gmail.com>
2025-04-30 19:20:11 +0200

    Clear old Express version to start fresh NestJS + NextJS version

--- app/(root)/(servers)/servers/[serverId]/page.tsx
+++
@@ -1,7 +0,0 @@
-import { notFound } from "next/navigation";
-
-const ServerPage = () => {
-  notFound();
-};
-
-export default ServerPage;
--- app/api/auth/[...nextauth]/route.tsx
+++
@@ -1,8 +0,0 @@
-import { authOptions } from "@/lib/session";
-import NextAuth from "next-auth";
-
-// Define the handler as NextAuth with authOptions
-const handler = NextAuth(authOptions);
-
-// Export the handler as GET and POST
-export { handler as GET, handler as POST };
