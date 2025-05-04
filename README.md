86878d4783ffb338e453010e3e143ba9ff1f0cd9
Dzenis <dzenisgudzevic18@gmail.com>
2025-05-04 16:01:37 +0200

    feat: implement user module with controller, service, and schema

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # Vord
 
-Vord is a real-time voice and chat platform inspired by Discord
+Vord is a real-time communication platform designed for the modern web. It enables seamless voice, video, and text collaboration in one unified interface. Built with scalable web technologies, Vord offers low-latency media streaming, persistent chat, and rich collaboration features — making it ideal for teams, communities, and developers building connected experiences.
---
+++ api/src/models/user/schema/user.schema.ts
@@ -0,0 +1,37 @@
+import { HydratedDocument } from 'mongoose';
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+
+export type UserDocument = HydratedDocument<User>;
+
+@Schema({
+  timestamps: true,
+})
+export class User {
+  @Prop({
+    type: String,
+    required: true,
+    minlength: 2,
+    maxlength: 15,
+    trim: true,
+    unique: true,
+  })
+  username: string;
+
+  @Prop({
+    type: String,
+    required: true,
+    minlength: 5,
+    maxlength: 255,
+    trim: true,
+    unique: true,
+  })
+  email: string;
+
+  @Prop({
+    type: Boolean,
+    default: false,
+  })
+  isGoogleAccount?: boolean;
+}
+
+export const UserSchema = SchemaFactory.createForClass(User);
